edf6c6b69998840fb71adb3f805c9bcbc7940ce8
RiimoH <remo.herzog@hotmail.com>
2021-12-11 15:28:03 +0100

    wtf

--- 11/11.py
+++ 11/11.py
@@ -3,7 +3,40 @@ from colorama import init, Fore
 def read_file(file):
     with open(file) as fp:
         inp = fp.read().split('\n')
+        inp = [[int(char) for char in row] for row in inp]
+        print(inp)
     return inp
+
+def part_one(inp):
+
+    total_flashes = 0
+
+
+    while True:
+        # step one: increase all values
+        for row in inp:
+            for octopuss in row:
+                octopuss += 1
+
+        # step two: flash all octopusses with a value higher than 9
+        flashed = True
+        
+        while flashed:
+            flashed = False
+            for row, line in enumerate(inp):
+                for col, octopuss in enumerate(line):
+                    if octopuss > 9:
+                        total_flashes += 1
+                        octopuss -= octopuss
+                        for dr, dc in ((1,0), (0,1), (0,-1), (-1,0)):
+                            cr = dr+row
+                            cc = dc+col
+                            if 0 <= cr < 10 and 0 <= cc < 10:
+                                if inp[cr][cc] > 0:
+                                    inp[cr][cc] += 1
+                                    flashed = True
+
+        yield inp
 
 
 
@@ -12,8 +45,14 @@ if __name__ == "__main__":
     init()
     
     # load inputs/test data
-    inp = read_file("input.txt")
-    test = read_file("test.txt")
+    inp = read_file("./input.txt")
+    test = read_file("./test.txt")
 
-    print("Part One Test: ", part_one(test))
-    print("Part One: ", part_one(inp))
+    p1 = part_one(test)
+    for _ in range(2):
+        next(p1)
+
+    
+    for row in test:
+        print(''.join(map(str, row)))
+        

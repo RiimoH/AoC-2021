4ef89b19c035d26473a0f78ff3e19e446e4fa8cb
RiimoH <remo.herzog@hotmail.com>
2021-12-13 10:09:38 +0100

    day 12 & 13 finished

--- 12/12.py
+++ 12/12.py
@@ -30,20 +30,49 @@ def part_one(pos, path, nodes, paths):
 
     return
 
+def part_two(pos, path, nodes, paths, double = False):
+    if pos == "end":
+        paths.append(tuple([*path, pos]))
+        return
+
+    for next_node in nodes[pos]:
+        if next_node.lower() != next_node or next_node not in path:
+            part_two(next_node, [*path, pos], nodes, paths, double = double)
+        elif next_node.lower() == next_node and not double and next_node != "start":
+            part_two(next_node, [*path, pos], nodes, paths, double = True)
+
+    return
+
+def setup_and_run_one(txt):
+    inp = read_file(txt)
+    nodes = create_nodes(inp)
+    paths = []
+    part_one("start", list(), nodes, paths)
+    return paths
+
+def setup_and_run_two(txt):
+    inp = read_file(txt)
+    nodes = create_nodes(inp)
+    paths = []
+    part_two("start", list(), nodes, paths)
+    return paths
 
 if __name__ == "__main__":
     # initiate colorama
     init()
 
-    # load inputs/test data
-    inp = read_file("./input.txt")
-    test = read_file("./test1.txt")
-
-    paths = []
-    nodes = create_nodes(test)
-    part_one("start", list(), nodes, list())
-    print("Part One TEST: ", paths)
-    # print("Part One: ", part_one(inp, 100))
-
-    # print("Part Two TEST: ", part_two(test) == 195)
-    # print("Part Two: ", part_two(inp))
+    # run tests
+    for test, target in [("test1.txt",10), ("test2.txt",19),("test3.txt",226),]:
+        paths = setup_and_run_one(test)
+        print("Test: ", len(paths) == target, len(paths))
+    
+    paths = setup_and_run_one("input.txt")
+    print("Part One: ", len(paths))
+    
+    # run tests
+    for test, target in [("test1.txt",36), ("test2.txt",103),("test3.txt",3509),]:
+        paths = setup_and_run_two(test)
+        print("Test: ", len(paths) == target, len(paths))
+    
+    paths = setup_and_run_two("input.txt")
+    print("Part Two: ", len(paths))

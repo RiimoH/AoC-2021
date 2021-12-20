e1f35379bdab7b0930aa67bdecd89326676507bb
RiimoH <remo.herzog@hotmail.com>
2021-12-20 23:44:59 +0100

    18 finished

--- 18/18_2.py
+++ 18/18_2.py
@@ -1,3 +1,4 @@
+from time import time
 def read_file(txt):
     with open(txt) as fp:
         inp = fp.read().split("\n")
@@ -81,92 +82,32 @@ def magnitude(depths, values):
                                      3+values[idx+1]*2] + values[idx+2:]
             depths = depths[:idx] + [d-1] + depths[idx+2:]
 
-    return depths, values
+    return values[0]
 
 
 if __name__ == "__main__":
-    tests = ["""[[[[4,3],4],4],[7,[[8,4],9]]]
-[1,1]""",
-             """[1,1]
-[2,2]
-[3,3]
-[4,4]""",
-             """[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]""",
-             """[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]
-[6,6]""",
-             """[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]""", ]
+    lines = list(map(parse, read_file("test.txt")))
 
-    for tn, test in enumerate(tests):
-        print("\n\n", tn)
-        lines = test.split("\n")
-        d, v = parse(lines[0])
-
-        for line in lines[1:]:
-            d, v = add(d, v, *parse(line))
+    max_mag = 0
+    st = time()
+    for l1 in lines:
+        for l2 in lines:
+        
+            d, v = add(*l1, *l2)
             d, v = reduce(d, v)
-        print(d, v)
-
-    magnitude_tests = ["[[1,2],[[3,4],5]]",
-                       "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]",
-                       "[[[[1,1],[2,2]],[3,3]],[4,4]]",
-                       "[[[[3,0],[5,3]],[4,4]],[5,5]]",
-                       "[[[[5,0],[7,4]],[5,5]],[6,6]]",
-                       "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"]
-
-    print("\n\nMagnitude Tests")
-    for mt in magnitude_tests:
-        print(mt)
-        print(magnitude(*parse(mt)))
-
-
-    print("\n\n Final Test")
-    test = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-[[[5,[2,8]],4],[5,[[9,9],0]]]
-[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-[[[[5,4],[7,7]],8],[[8,3],8]]
-[[9,3],[[9,9],[6,[4,9]]]]
-[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"""
-    lines = test.split("\n")
-    d, v = parse(lines[0])
-
-    for line in lines[1:]:
-        d, v = add(d, v, *parse(line))
-        d, v = reduce(d, v)
-    print(d, v)
-    print(magnitude(d,v))   
+            max_mag = max(max_mag, magnitude(d, v))
+    print(max_mag)
+    print(time()-st)
     
-    
-    
-    print("\n\n Part One")
-    lines = read_file("input.txt")
-    d, v = parse(lines[0])
+    lines = list(map(parse, read_file("input.txt")))
 
-    for line in lines[1:]:
-        d, v = add(d, v, *parse(line))
-        d, v = reduce(d, v)
-    print(d, v)
-    print(magnitude(d,v))
-
-
-    
+    max_mag = 0
+    st = time()
+    for l1 in lines:
+        for l2 in lines:
+        
+            d, v = add(*l1, *l2)
+            d, v = reduce(d, v)
+            max_mag = max(max_mag, magnitude(d, v))
+    print(max_mag)
+    print(time()-st)
